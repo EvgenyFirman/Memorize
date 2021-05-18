d36a17e31397a1762431a70da30c95e4a8c9d767
Eugene <Eugeniolio@yandex.com>
2021-05-18 23:48:17 +0300

    base project

---
+++ MemorizeApp/MemorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppApp.swift
+//  MemorizeApp
+//
+//  Created by Евгений Фирман on 18.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
