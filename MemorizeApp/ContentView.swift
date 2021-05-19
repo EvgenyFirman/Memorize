8e8ba69a1eef8969decace546540e3128b019665
Eugene <Eugeniolio@yandex.com>
2021-05-19 14:31:59 +0300

    interface base

--- MemorizeApp/ContentView.swift
+++ MemorizeApp/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return HStack{
+            ForEach(0..<4) {_ in
+                ItemView()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ItemView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0).padding()
+                Text("☠️").font(Font.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.orange).padding()
+            }
+            
+        }.foregroundColor(Color.orange)
     }
 }
